34aae8516230a9a67e692b755ab7f02aa21cbf51
torbjorn-sorman <torbjorn.sorman@gmail.com>
2015-09-28 16:48:24 +0200

    started on opencl

--- Code/OpenCL/opencl_fft/fftGPUSync.cpp
+++ Code/OpenCL/opencl_fft/fftGPUSync.cpp
@@ -1,2 +1,19 @@
 #include "fftGPUSync.h"
 
+void setup();
+
+int     GPUSync_validate(cInt n)
+{
+    setup();
+    return 1;
+}
+
+double  GPUSync_performance(cInt n)
+{
+    return -1.0;
+}
+
+void setup()
+{
+    
+}
--- Code/OpenCL/opencl_fft/main.cpp
+++ Code/OpenCL/opencl_fft/main.cpp
@@ -2,7 +2,8 @@
 
 int main()
 {
-    printf("Success!\n");
+    
+    printf("Done\n");
     getchar();
     return 0;
 }

fdbda01ec9c8d0c9490836c68a578dcc7f6b57dc
Torbjörn Sörman <torbjorn.sorman@gmail.com>
2015-12-06 22:41:29 +0100

    small changes

--- Code/BenchmarkFFT/benchmarkfft/Definitions.cpp
+++ Code/BenchmarkFFT/benchmarkfft/Definitions.cpp
@@ -3,7 +3,7 @@
 int vendor_gpu = VENDOR_NVIDIA;
 int number_of_tests = 8;
 #if defined(_NVIDIA)
-int batch_total_points = 1048576;//67108864;
+int batch_total_points = 67108864;
 #elif defined(_AMD)
 int batch_total_points = 67108864;
 #endif
